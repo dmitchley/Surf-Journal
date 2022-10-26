a4b3382aa9fdbc08092fbe4eb20952d165efaaad
Dylan Mitchley <[dylanmitchley1994@gmail.com]>
2022-10-26 19:34:18 +0200

    Deployed Backend

--- frontend/src/tests/Journal.test.js
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "vitest";
-import { journals, url } from "../pages/Journal";
-// comparing the data and url and if its is equal the api is working.
-test("Test API", async () => {
-  await url;
-  expect(url).toEqual(journals);
-});
